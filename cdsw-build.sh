112bd70de4f3a34639e8e9b87b6d6af3039a7f4d
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2020-10-20 13:44:04 -0400

    Update cdsw-build.sh

--- cdsw-build.sh
+++ cdsw-build.sh
@@ -1,3 +1,5 @@
+pip3 install --upgrade pip
+
 #pip3 install sklearn
 #pip3 install textblob
 # http://www.nltk.org/install.html
@@ -6,3 +8,4 @@ pip3 install -U nltk
 #pip3 install -U numpy
 
 python3 -m nltk.downloader popular
+python3 -m nltk.downloader vader_lexicon
